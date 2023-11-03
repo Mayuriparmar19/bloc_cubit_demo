c64c584aa58e28f182adb39856af06c2185aeeb8
System 16 <mittuparmar19@gmail.com>
2023-11-03 13:14:28 +0530

    Internet cubit added and connectivity,cancel method added into internet cubit class.

--- lib/Cubit/internet_cubit.dart
+++ lib/Cubit/internet_cubit.dart
@@ -1,5 +1,29 @@
-enum InternetState{
-          initial,lost,gained
+import 'dart:async';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+enum InternetState { initial, lost, gained }
+
+class InternetCubit extends Cubit<InternetState> {
+  Connectivity connectivity = Connectivity();
+  StreamSubscription? connectivitySubscription;
+
+  InternetCubit() : super(InternetState.initial) {
+    connectivitySubscription =
+        connectivity.onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        emit(InternetState.gained);
+      } else {
+        emit(InternetState.lost);
+      }
+    });
+  }
+  @override
+  Future<void> close() {
+    connectivitySubscription?.cancel();
+    return super.close();
+  }
 }
 //if your state have nothing is the classes then you can declare them as a enum.
 // you can use enum only and only when you have empty classes.
